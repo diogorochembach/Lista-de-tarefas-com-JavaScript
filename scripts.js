775346616c1246078e66e35818ab0f2d88195f0b
diogorochembach <diogo.iju@gmail.com>
2023-03-15 09:45:55 -0300

    Finalização aplicação JavaScript

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 const inputElement = document.querySelector(".new-task-input");
 const addTaskButton = document.querySelector(".new-task-button");
 
+const tasksContainer = document.querySelector(".tasks-container");
+
 const validateInput = () => {
   if (inputElement.value.trim().length > 0) {
     return true;
@@ -15,8 +17,53 @@ const validateInput = () => {
 const handleAddTask = () => {
   const inputIsValid = validateInput();
 
+  console.log(inputIsValid);
+
   if (!inputIsValid) {
     return inputElement.classList.add("error");
+  }
+
+  const taskItemContainer = document.createElement("div");
+  taskItemContainer.classList.add("task-item");
+
+  const taskContent = document.createElement("p");
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent));
+
+  const deleteItem = document.createElement("i");
+  deleteItem.classList.add("far");
+  deleteItem.classList.add("fa-trash-alt");
+
+  deleteItem.addEventListener("click", () =>
+    handleDeleteClick(taskItemContainer, taskContent)
+  );
+
+  taskItemContainer.appendChild(taskContent);
+  taskItemContainer.appendChild(deleteItem);
+
+  tasksContainer.appendChild(taskItemContainer);
+
+  inputElement.value = "";
+};
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    if (task.firstChild.isSameNode(taskContent)) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+};
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    if (task.firstChild.isSameNode(taskContent)) {
+      taskItemContainer.remove();
+    }
   }
 };
 
